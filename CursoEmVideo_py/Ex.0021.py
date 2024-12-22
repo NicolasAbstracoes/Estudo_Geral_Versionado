42749abc04574d6b403243c1866d583d941c0a43
Nicolas Cesar <nicolasabstracoes@gmail.com>
2024-12-21 21:46:42 -0300

    Ex.0021
    Importei outra biblioteca e fiz outro caminho, apesar de simples. E me irritei um pouco porque não estava entendendo o caminho correto do arquivo. Porque não usei o caminho relativo, e tambem funciona com url.

---
+++ CursoEmVideo_py/Ex.0021.py
@@ -0,0 +1,6 @@
+# Tentando tocar um audio mp3, me senti burro porque o negocio nao estava funcionado, porque eu estava usando o caminho todo nao o caminho relativo.
+
+import playsound
+
+#https://www.tuningmania.com.br/autosom/mp3/10%20%7E%2014%20Hz.MP3
+playsound.playsound("CursoEmVideo_py\Ex0021.mp3")
