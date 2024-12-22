44a91a6c82dc4409dfc5e413850c0489fb1d0638
Nicolas Cesar <nicolasabstracoes@gmail.com>
2024-12-22 16:11:57 -0300

    Update Ex.0023.py
    Acrescentei a separação de digitos com o dado em str

--- CursoEmVideo_py/Ex.0023.py
+++ CursoEmVideo_py/Ex.0023.py
@@ -1,6 +1,7 @@
 numero_original = int(input('Digite um número entre 0 e 9999: '))
 
-# Separar os dígitos
+#Separa os numeros de forma matematica.
+
 milhar = numero_original // 1000
 centena = (numero_original % 1000) // 100
 dezena = (numero_original % 100) // 10
@@ -9,3 +10,11 @@ print(milhar)
 print(centena)
 print(dezena)
 print(unidade)
+
+#Mesmo processo mas com input como str
+numero_original_str = str(input('Digite um número entre 0 e 9999: '))
+
+print(numero_original_str[0])
+print(numero_original_str[1])
+print(numero_original_str[2])
+print(numero_original_str[3])
